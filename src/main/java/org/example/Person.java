f68031fba84533ecff475b89f63fb4f01c436880
илья <126683197+varfolomeevilya@users.noreply.github.com>
2023-09-27 11:55:31 +0300

    Update Person.java

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,21 +1,50 @@
 package org.example;
 
-public class Person extends Firms {
-    private String fio;
+abstract public class Person extends Firms {
+     private String surname;
+    private String name;
+    private String nativeland;
+
     public Person(){
-        this.fio = "Ivanov";
-
-    }
-    public Person(String fio){
-        this.fio = fio;
-
-    }
-    public String getFio() {
-        return fio;
     }
 
-    public void setFio(String fio) {
-        this.fio = fio;
+    public Person(String surname, String name, String nativeland){
+        this.surname=surname;
+        this.name=name;
+        this.nativeland=nativeland;
     }
 
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getNativeland() {
+        return nativeland;
+    }
+
+    public void setNativeland(String nativeland) {
+        this.nativeland = nativeland;
+    }
+    public Float takeSalary(){
+        return null;
+    }
+
+      @Override
+      public String toString() {
+          return "Person{" +
+                  "surname='" + surname + '\'' +
+                  ", name='" + name + '\'' +
+                  ", nativeland='" + nativeland + '\'' +
+                  '}';
+      }
 }
