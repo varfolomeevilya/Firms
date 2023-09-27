1cb30dcf1cea89ce7b95c8516b36e133ed08e5ff
илья <126683197+varfolomeevilya@users.noreply.github.com>
2023-09-27 11:53:25 +0300

    Update Firms.java

--- src/main/java/org/example/Firms.java
+++ src/main/java/org/example/Firms.java
@@ -2,49 +2,22 @@ package org.example;
 
 public class Firms  {
 
-    private int bids;
-    private int hourly;
-    private int piecework;
-    private int total;
+    ArrayList<Person> persons;
+
     public Firms(){
-        this.bids= 3000;
-        this.hourly=800;
-        this.piecework=5500;
-    }
-    public Firms(int bids, int hourly, int piecework){
-        this.bids=bids;
-        this.hourly=hourly;
-        this.piecework=piecework;
-    }
-    public int getBids() {
-        return bids;
-    }
-    public int getTotal(){
-        return total;
-    }
-    public int getTotal(Firms firms) {
-        return firms.getBids()+ firms.getHourly()+ firms.getPiecework();
     }
 
-    public void setTotal(int total) {
-        this.total = total;
-    }
-    public void setBids(int bids) {
-        this.bids = bids;
-    }
-    public int getHourly() {
-        return hourly;
+    public Firms(ArrayList<Person> persons){
+       this.persons = persons;
     }
 
-    public void setHourly(int hourly) {
-        this.hourly = hourly;
+    public Float calcSalary(){
+        Float sum=0.0f;
+        for(Person person : persons){
+            sum+= person.takeSalary();
+        }
+        return sum;
     }
-    public int getPiecework() {
-        return piecework;
-    }
-
-    public void setPiecework(int piecework) {
-        this.piecework = piecework;
-    }
-
+   
+   
 }
